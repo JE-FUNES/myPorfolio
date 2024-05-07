db9907dd819549f7068c95ce9ac9ee15ed89061f
JE-FUNES <infoventacba@gmail.com>
2024-05-07 14:22:27 -0300

    actualizacion

--- client/src/components/Contact/ContactData.js
+++ client/src/components/Contact/ContactData.js
@@ -6,7 +6,7 @@ export const ContactData = [
         Bg_color: "bg-[#e9d5ff]",
         icon: <FaPhoneAlt/>,
         name: "PHONE",
-        contact: "+54 9 351 2737 199",
+        contact: " +54 9 351 2737 199",
         key: "phone",
         link: "tel:+5493512737199"
     },
@@ -14,7 +14,7 @@ export const ContactData = [
         Bg_color: "bg-[#c084fc]",
         icon: <FaEnvelope/>,
         name: "MAIL",
-        contact: "juliafunesaragon@gmail.com",
+        contact: " juliafunesaragon@gmail.com",
         key: "mail",
         link: "mailto: juliafunesaragon@gmail.com"
     },
