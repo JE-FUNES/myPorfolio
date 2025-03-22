c0196014fb4c43a33c2856b90346ecdb8790c4cd
JE-FUNES <infoventacba@gmail.com>
2025-03-22 11:49:07 -0300

    function closeMenu

--- src/components/Recruiters/Navbar/Navbar.js
+++ src/components/Recruiters/Navbar/Navbar.js
@@ -11,49 +11,98 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   const language = useSelector((state) => state.language);
 
+  const closeMenu = () => {
+    setOpen(false); 
+  };
 
   return (
     <>
       <React.Fragment>
-    
-      {/*----- Start Navbar -----*/}
-      <header>
-        <nav className="fixed top-0 w-full z-40 bg-white">
-          <div className="container sm:py-3 xl:py-5">
-            <div className="flex items-center justify-between md:block">
-              <div className="flex justify-between items-center w-full">
-                {/* Logo */}
-                <div className="z-50 relative">
-                <a href={"https://www.jefa-web.com.ar/"}>
-                    <div className="flex flex-col items-center">
-                      <img
-                        src={logo}
-                        alt="Web Developer"
-                        title="Web Developer"
-                        className="lg:px-2 sm:max-w-[120px] lg:max-w-[150px] xl:ml-5 xl:max-w-[200px]"
-                      />
-                      <p className="text-purple-800/70 pl-4 pt-1 -mb-2 sm:text-xs lg:text-sm">
-                      {language === "en" 
-                    ? "You are in the IT Recruiters section." 
-                    : "Estás en la sección Reclutadores IT"}
+        {/*----- Start Navbar -----*/}
+        <header>
+          <nav className="fixed top-0 w-full z-40 bg-white">
+            <div className="container sm:py-3 xl:py-5">
+              <div className="flex items-center justify-between md:block">
+                <div className="flex justify-between items-center w-full">
+                  {/* Logo */}
+                  <div className="z-50 relative">
+                    <a href={"https://www.jefa-web.com.ar/"}>
+                      <div className="flex flex-col items-center">
+                        <img
+                          src={logo}
+                          alt="Web Developer"
+                          title="Web Developer"
+                          className="lg:px-2 sm:max-w-[120px] lg:max-w-[150px] xl:ml-5 xl:max-w-[200px]"
+                        />
+                        <p className="text-purple-800/70 pl-4 pt-1 -mb-2 sm:text-xs lg:text-sm">
+                          {language === "en"
+                            ? "You are in the IT Recruiters section."
+                            : "Estás en la sección Reclutadores IT"}
                         </p>
-                    </div>
-                  </a>
+                      </div>
+                    </a>
+                  </div>
+                  {/* Button close and menu */}
+                  <div className="flex items-center gap-5 lg:hidden">
+                    <button
+                      onClick={() => setOpen(!open)}
+                      className="block lg:hidden text-[40px] outline-none text-fuchsia-500"
+                    >
+                      {open ? <RiCloseFill /> : <RiMenu3Fill />}
+                    </button>
+                  </div>
+                  {/* Desktop screen */}
+                  <div className="hidden lg:block">
+                    <ul className="list-none flex space-x-3 items-end right-0 w-full cursor-pointer">
+                      {language === "en"
+                        ? NavbarData.map((e, key) => (
+                            <li key={key}>
+                              <Link
+                                to={e.link}
+                                smooth={true}
+                                duration={500}
+                                className="inline-block px-1.5 lg:text-sm xl:text-base font-medium uppercase leading-9 tracking-[2px] hover:text-pink-500"
+                              >
+                                {e.name}
+                              </Link>
+                            </li>
+                          ))
+                        : EspNavbarData.map((e, key) => (
+                            <li key={key}>
+                              <Link
+                                to={e.link}
+                                smooth={true}
+                                duration={500}
+                                className="inline-block px-1.5 lg:text-sm xl:text-base font-medium uppercase leading-9 tracking-[2px] hover:text-pink-500"
+                              >
+                                {e.name}
+                              </Link>
+                            </li>
+                          ))}
+                      <div className="lg:pl-10 xl:pl-3 lg:mr-4 xl:mr-2">
+                        <a
+                          href={"https://wa.me/+5493512737199"}
+                          target="blank"
+                          className="btn btn-whapp  shadow-md shadow-black"
+                          title="Whatsapp"
+                        >
+                          <FaWhatsapp className="inline-block w-6 h-6 px-0 my-1" />
+                        </a>
+                      </div>
+                      <div className="lg:pl-4 xl:px-2">
+                        <LanguagesSwitcher />
+                      </div>
+                    </ul>
+                  </div>
                 </div>
-                {/* Button close and menu */}
-                <div className="flex items-center gap-5 lg:hidden">
-                  <button
-                    onClick={() => setOpen(!open)}
-                    className="block lg:hidden text-[40px] outline-none text-fuchsia-500"
-                  >
-                    {open ? <RiCloseFill /> : <RiMenu3Fill />}
-                  </button>
+              </div>
+              {/* Mobile screen */}
+              <div className={`lg:hidden ${open ? "" : "hidden"}`}>
+                <div className="mt-4">
+                  <LanguagesSwitcher />
                 </div>
-                {/* Desktop screen */}
-                <div className="hidden lg:block">
-                
-                  <ul className="list-none flex space-x-3 items-end right-0 w-full cursor-pointer">
-                    
+                <div className="pt-5 ml-5">
+                  <ul>
                     {language === "en"
                       ? NavbarData.map((e, key) => (
                           <li key={key}>
@@ -61,7 +110,8 @@ const Navbar = () => {
                               to={e.link}
                               smooth={true}
                               duration={500}
-                              className="inline-block px-1.5 lg:text-sm xl:text-base font-medium uppercase leading-9 tracking-[2px] hover:text-pink-500"
+                              onClick={closeMenu}
+                              className="inline-block px-1.5 text-base font-medium uppercase leading-9 tracking-[2px] hover:text-orange-600"
                             >
                               {e.name}
                             </Link>
@@ -73,88 +123,30 @@ const Navbar = () => {
                               to={e.link}
                               smooth={true}
                               duration={500}
-                              className="inline-block px-1.5 lg:text-sm xl:text-base font-medium uppercase leading-9 tracking-[2px] hover:text-pink-500"
+                              onClick={closeMenu}
+                              className="inline-block px-1.5 text-base font-medium uppercase leading-9 tracking-[2px] hover:text-orange-600"
                             >
                               {e.name}
                             </Link>
                           </li>
                         ))}
-                    <div className="lg:pl-10 xl:pl-3 lg:mr-4 xl:mr-2">
-                      <a
-                        href={"https://wa.me/+5493512737199"}
-                        target="blank"
-                        className="btn btn-whapp  shadow-md shadow-black"
-                        title="Whatsapp"
-                      >
-                        <FaWhatsapp className="inline-block w-6 h-6 px-0 my-1" />
-                      </a>
-                    </div>
-                    <div className="lg:pl-4 xl:px-2">
-                      <LanguagesSwitcher />
-                    </div>
+                    <a
+                      href={"https://wa.me/+5493512737199"}
+                      target="blank"
+                      className="btn btn-whapp  text-base w-[170px] shadow-md shadow-black"
+                      title="Whatsapp"
+                    >
+                      <FaWhatsapp className="inline-block w-6 h-6 px-0 my-1 mr-2" />
+                      WHATSAPP
+                    </a>
                   </ul>
                 </div>
               </div>
             </div>
-            {/* Mobile screen */}
-            <div className={`lg:hidden ${open ? "" : "hidden"}`}>
-            <div className="mt-4">
-                    <LanguagesSwitcher />
-                  </div>
-              <div className="pt-5 ml-5">
-                <ul>
-                    {language === "en"
-                    ?
-
-                    (
-
-                        NavbarData.map((e, key) => (
-                            <li key={key}>
-                      <Link
-                        to={e.link}
-                        smooth={true}
-                        duration={500}
-                        className="inline-block px-1.5 text-base font-medium uppercase leading-9 tracking-[2px] hover:text-orange-600"
-                        >
-                        {e.name}
-                      </Link>
-                    </li>
-                  ))
-                ) : (
-
-                    EspNavbarData.map((e, key) => (
-                        <li key={key}>
-                  <Link
-                    to={e.link}
-                    smooth={true}
-                    duration={500}
-                    className="inline-block px-1.5 text-base font-medium uppercase leading-9 tracking-[2px] hover:text-orange-600"
-                    >
-                    {e.name}
-                  </Link>
-                </li>
-              ))
-            )
-            }
-                  <a
-                    href={"https://wa.me/+5493512737199"}
-                    target="blank"
-                    className="btn btn-whapp  text-base w-[170px] shadow-md shadow-black"
-                    title="Whatsapp"
-                  >
-                    <FaWhatsapp className="inline-block w-6 h-6 px-0 my-1 mr-2" />
-                    WHATSAPP
-                  </a>
-                  
-                </ul>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-      {/*----- End Navbar -----*/}
+          </nav>
+        </header>
+        {/*----- End Navbar -----*/}
       </React.Fragment>
-  
     </>
   );
 };
